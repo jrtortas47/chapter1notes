95c72e9f436e07dbc5a8e2469d23c00ee52d64bf
wj5w <wj5w>
2023-02-06 09:52:20 -0800

    initial commit

---
+++ datatypes.js
@@ -0,0 +1,35 @@
+console.log(7);
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+console.log(0);
+console.log(3);
+//Floating point Number
+console.log(3.14);
+console.log(100.045);
+
+//Strings
+//Characters, letters, symbol, spaces
+console.log("hello world");
+console.log('hello world!!');
+console.log(`hello world 123`);
+console.log('16.0');
+
+//Arithmetic Operators (Math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //shift-8 gives us*
+console.log(14/2);
+console.log(14%2); //is called MOD and means remainder after dividing
+console.log(15%10);
+
+//concatenation: combine strings
+console.log("my name"+"is gerardo Perez.");
+console.log('My'+'+ age' + '+''is'+''+15+'.');
+console.log("your age is\n17.");
+
+//Template literal-plug in a calculated value.
+//use backticks " next to the 1
+console.log('15 divided by 10=${15/10} axcvzxcvx');
---
+++ operators.js
@@ -0,0 +1,13 @@
+//unary operators use only one value
+console.log(typeof 15);
+console.log(typeof "this strings");
+console.log(typeof true);
+console.log(typeof false);
+
+//binary Operators use two values
+//comparison Operators
+console.log(10>5); //greater than
+console.log(10<5); //less than
+console.log(10>=5); //greater than or equal
+
+//Ternary Operators use three values
